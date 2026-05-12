82bb6f17f5148847ecc5ed8f74c984ca3945e17a
seif <seifeldeinh@gmail.com>
2026-05-13 00:49:00 +0300

    Checking on the modified old version of the smartHome Project.util.EventBus.Java

---
+++ src/util/EventBus.java
@@ -0,0 +1,47 @@
+package util;
+
+import jdk.jfr.Event;
+import jdk.jfr.EventType;
+import model.hub.DeviceEvent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+
+public final class EventBus {
+    private static volatile EventBus instance;
+
+    private final Map<EventType, List<Consumer<DeviceEvent>>> listenersByType = new ConcurrentHashMap<>();
+
+    private EventBus() {
+    }
+
+    public static EventBus getInstance() {
+        if (instance == null) {
+            synchronized (EventBus.class) {
+                if (instance == null) {
+                    instance = new EventBus();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void subscribe(EventType type, Consumer<DeviceEvent> listener) {
+        listenersByType
+                .computeIfAbsent(type, key -> new CopyOnWriteArrayList<>())
+                .add(listener);
+    }
+
+    public void publish(DeviceEvent event) {
+        List<Consumer<DeviceEvent>> listeners = listenersByType.get(event.getEventType());
+        if (listeners == null) {
+            return;
+        }
+        for (Consumer<DeviceEvent> listener : listeners) {
+            listener.accept(event);
+        }
+    }
+}
