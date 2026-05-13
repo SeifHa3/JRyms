d85916efd400452df1c7ce65a8d0f5c860553c09
seif <seifeldeinh@gmail.com>
2026-05-13 05:36:10 +0300

    main1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.stage.Stage;
+import model.device.SmartAC;
+import model.hub.DeviceRegistry;
+import service.CommandService;
+import service.DeviceService;
+import controller.DashboardController;
+import view.DashboardView;
+import view.ViewManager;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        ViewManager.getInstance().init(primaryStage);
+        bootstrapDevices();
+
+        DeviceService deviceService = new DeviceService();
+        CommandService commandService = new CommandService();
+
+        DashboardView dashboardView = new DashboardView();
+        DashboardController controller = new DashboardController(deviceService, commandService);
+        controller.init(dashboardView);
+
+        ViewManager.getInstance().showDashboard();
+    }
+
+    private void bootstrapDevices() {
+        DeviceRegistry registry = DeviceRegistry.getInstance();
+        SmartAC ac1 = new SmartAC("ac1", "Living Room AC");
+        SmartAC ac2 = new SmartAC("ac2", "Bedroom AC");
+        registry.register(ac1);
+        registry.register(ac2);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
