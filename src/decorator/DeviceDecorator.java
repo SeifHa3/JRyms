a3c474ca2c291a879461c1c736ee4f024d319f63
seif <seifeldeinh@gmail.com>
2026-05-13 06:50:41 +0300

    Applying Decorator

---
+++ src/decorator/DeviceDecorator.java
@@ -0,0 +1,45 @@
+package decorator;
+
+import model.command.Command;
+import model.device.DeviceObserver;
+import model.device.DeviceStatus;
+import model.device.SmartDevice;
+import model.hub.DeviceEvent;
+import model.room.Room;
+
+public abstract class DeviceDecorator extends SmartDevice {
+
+    protected final SmartDevice wrapped;
+
+    public DeviceDecorator(SmartDevice wrapped) {
+        super(wrapped.getId(), wrapped.getName());
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public void executeCommand(Command c) {
+        wrapped.executeCommand(c);
+    }
+
+    @Override
+    public String getStatusSummary() {
+        return wrapped.getStatusSummary();
+    }
+
+    @Override
+    public Room getRoom()                        { return wrapped.getRoom(); }
+    @Override
+    public void setRoom(Room room)               { wrapped.setRoom(room); }
+    @Override
+    public DeviceStatus getStatus()              { return wrapped.getStatus(); }
+    @Override
+    public void setStatus(DeviceStatus status)   { wrapped.setStatus(status); }
+    @Override
+    public String getName()                      { return wrapped.getName(); }
+    @Override
+    public void setName(String name)             { wrapped.setName(name); }
+    @Override
+    public void addObserver(DeviceObserver o)    { wrapped.addObserver(o); }
+    @Override
+    public void removeObserver(DeviceObserver o) { wrapped.removeObserver(o); }
+}
---
+++ src/factory/DecoratorType.java
@@ -0,0 +1,6 @@
+package factory;
+
+public enum DecoratorType {
+    LOGGING,
+    ACCESS_CONTROL
+}
