4e99ddd14c1119cdeaacb95ed8d8c8e56f0a2c30
seif <seifeldeinh@gmail.com>
2026-05-13 05:22:00 +0300

    managerView

---
+++ src/view/ViewManager.java
@@ -0,0 +1,46 @@
+package view;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.device.SmartAC;
+
+public class ViewManager {
+
+    private static volatile ViewManager instance;
+    private Stage primaryStage;
+
+    private ViewManager() {}
+
+    public static ViewManager getInstance() {
+        if (instance == null) {
+            synchronized (ViewManager.class) {
+                if (instance == null) {
+                    instance = new ViewManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void init(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+    public void showDashboard() {
+        DashboardView view = new DashboardView();
+        Scene scene = new Scene(view.build(), 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(util.AppConstants.APP_NAME);
+        primaryStage.show();
+    }
+
+    public void showACControl(SmartAC ac) {
+        // M4 will implement ACControlView
+        // placeholder for now
+    }
+
+    public void showAddDevice() {
+        // M5 will implement AddDeviceFlow
+        // placeholder for now
+    }
+}
